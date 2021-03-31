965e4eea433bf71b25eeab278077951450f3cfae
Jacob Bare <jacob@parameter1.com>
2021-03-31 10:37:47 -0500

    Use uppercase value and description
    To match Omeda’s prop naming conventions

--- packages/api-client/src/types-and-codes/email-contact-type.js
+++ packages/api-client/src/types-and-codes/email-contact-type.js
@@ -9,8 +9,8 @@ const values = Object.keys(descriptions).map((code) => parseInt(code, 10));
 class EmailContactType {
   constructor(value) {
     const v = parseInt(value, 10);
-    this.value = values.includes(v) ? v : 0;
-    this.description = descriptions[this.value];
+    this.Value = values.includes(v) ? v : 0;
+    this.Description = descriptions[this.Value];
   }
 }
 
