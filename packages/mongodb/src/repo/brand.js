e25a8b4eb36f0791d4dc7cefd5b627207d08c6db
Jacob Bare <jacob@parameter1.com>
2021-04-02 07:59:10 -0500

    Create brand upsert method

--- packages/mongodb/src/repo/brand.js
+++ packages/mongodb/src/repo/brand.js
@@ -1,4 +1,6 @@
 const { Repo } = require('@parameter1/mongodb/repo');
+const Joi = require('@parameter1/joi');
+const { validateAsync } = require('@parameter1/joi/utils');
 
 class BrandRepo extends Repo {
   /**
@@ -11,6 +13,28 @@ class BrandRepo extends Repo {
       dbName,
       client,
     });
+  }
+
+  /**
+   *
+   * @param {object} params
+   * @param {string} params.brand The brand abbreviation.
+   * @param {object} params.data The data to upsert.
+   */
+  async upsert(params = {}) {
+    const { brand, data } = await validateAsync(Joi.object({
+      brand: Joi.string().lowercase().trim().required(),
+      data: Joi.object().required(),
+    }).required(), params);
+
+    const now = new Date();
+    const query = { _id: brand };
+    const toUpsert = ['Id', 'Description', 'BrandAbbrev', 'CustomerCount'].reduce((o, key) => ({ ...o, [key]: data[key] }), {});
+    const update = {
+      $setOnInsert: { ...query, createdAt: now },
+      $set: { data: toUpsert, updatedAt: now },
+    };
+    return this.updateOne({ query, update, options: { upsert: true } });
   }
 }
 
