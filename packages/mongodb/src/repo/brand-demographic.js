7951f30f75dcfa501c44ddd911003c4ab1f33614
Jacob Bare <jacob@parameter1.com>
2021-04-07 15:52:19 -0500

    Add findValueById method to demographic repo

--- packages/mongodb/src/repo/brand-demographic.js
+++ packages/mongodb/src/repo/brand-demographic.js
@@ -22,6 +22,18 @@ class BrandDemographicRepo extends OmedaRepo {
     return doc ? doc.data : null;
   }
 
+  async findValueById({ demographicId, valueId } = {}) {
+    const pipeline = [];
+    pipeline.push({ $match: { 'data.Id': demographicId, brand: this.brandKey } });
+    pipeline.push({ $project: { values: '$data.DemographicValues' } });
+    pipeline.push({ $unwind: '$values' });
+    pipeline.push({ $match: { 'values.Id': valueId } });
+    pipeline.push({ $replaceRoot: { newRoot: '$values' } });
+    const cursor = await this.aggregate({ pipeline });
+    const [result] = await cursor.toArray();
+    return result;
+  }
+
   /**
    *
    * @param {object} params
