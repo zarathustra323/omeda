1ff2fc01960b5050bde146a054e7d1538e73f616
Jacob Bare <jacob@parameter1.com>
2021-05-26 11:48:30 -0500

    Remove format date directive

--- services/graphql/src/definitions/index.js
+++ services/graphql/src/definitions/index.js
@@ -1,5 +1,4 @@
 const { gql } = require('apollo-server-express');
-const formatDateDirectives = require('@parameter1/graphql-directive-format-date/directives');
 
 const codesAndTypes = require('./codes-and-types');
 const customer = require('./customer');
@@ -8,7 +7,6 @@ const deployment = require('./deployment');
 
 module.exports = gql`
 
-${formatDateDirectives.typeDefs}
 directive @brandData on FIELD_DEFINITION
 directive @codeOrType(instance: String!, path: String) on FIELD_DEFINITION
 directive @apiValue(path: String, as: ApiValueDirectiveAsEnum) on FIELD_DEFINITION
--- services/graphql/src/directives/index.js
+++ services/graphql/src/directives/index.js
@@ -1,10 +1,8 @@
-const formatDateDirectives = require('@parameter1/graphql-directive-format-date/directives');
 const BrandDataDirective = require('./brand-data');
 const CodeOrTypeDirective = require('./code-or-type');
 const ApiValueDirective = require('./api-value');
 
 module.exports = {
-  ...formatDateDirectives.classes,
   brandData: BrandDataDirective,
   codeOrType: CodeOrTypeDirective,
   apiValue: ApiValueDirective,
