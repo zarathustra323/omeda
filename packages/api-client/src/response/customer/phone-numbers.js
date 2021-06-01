91d38343c85d7487a35068bbf9c2f7cc883fbc5d
Jacob Bare <jacob@parameter1.com>
2021-06-01 09:16:53 -0500

    Add `getPrimary` phone number method

--- packages/api-client/src/response/customer/phone-numbers.js
+++ packages/api-client/src/response/customer/phone-numbers.js
@@ -11,6 +11,22 @@ class CustomerPhoneNumbersResponse extends ApiResourceResponse {
     const data = response.getAsArray('PhoneNumbers').map((obj) => new CustomerPhoneEntity(obj));
     super({ data, response });
   }
+
+  /**
+   * Loads the primary phone number for the this response.
+   * Will use the first business phone found (code 200),
+   * otherwise will fall back to the first number on the record.
+   *
+   * If no phone numbers are set, this will return `null`.
+   *
+   * @returns {CustomerPhoneEntity|null} The primary phone entity or null.
+   */
+  getPrimary() {
+    const { data } = this;
+    if (!data.length) return null;
+    const primary = data.find(({ PhoneContactType }) => PhoneContactType === 200);
+    return primary || data[0];
+  }
 }
 
 module.exports = CustomerPhoneNumbersResponse;
