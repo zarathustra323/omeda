ac62199feb6ad34e76d6b0cd39310d32bcbcbc52
Jacob Bare <jacob@parameter1.com>
2021-06-01 09:16:47 -0500

    Add `getPrimary` email address method

--- packages/api-client/src/response/customer/emails.js
+++ packages/api-client/src/response/customer/emails.js
@@ -11,6 +11,22 @@ class CustomerEmailsResponse extends ApiResourceResponse {
     const data = response.getAsArray('Emails').map((obj) => new CustomerEmailEntity(obj));
     super({ data, response });
   }
+
+  /**
+   * Loads the primary email address for the email response.
+   * Will use the first primary (business) email address found (code 300),
+   * otherwise will fall back to the first address on the record.
+   *
+   * If no email addresses are set, this will return `null`.
+   *
+   * @returns {CustomerEmailEntity|null} The primary email address entity or null.
+   */
+  getPrimary() {
+    const { data } = this;
+    if (!data.length) return null;
+    const primary = data.find(({ EmailContactType }) => EmailContactType === 300);
+    return primary || data[0];
+  }
 }
 
 module.exports = CustomerEmailsResponse;
