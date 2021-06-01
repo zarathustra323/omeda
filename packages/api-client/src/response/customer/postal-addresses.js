467b3e3932100b4e89a2a7173bea4bd321d05fc3
Jacob Bare <jacob@parameter1.com>
2021-06-01 09:20:54 -0500

    Add `getPrimary` postal address method

--- packages/api-client/src/response/customer/postal-addresses.js
+++ packages/api-client/src/response/customer/postal-addresses.js
@@ -11,6 +11,25 @@ class CustomerPostalAddressesResponse extends ApiResourceResponse {
     const data = response.getAsArray('Addresses').map((obj) => new CustomerPostalAddressEntity(obj));
     super({ data, response });
   }
+
+  /**
+   * Loads the primary postal address for this response.
+   * Will use the first business address found (code 100),
+   * then will check for an address with a company name,
+   * otherwise will fall back to the first address on the record.
+   *
+   * If no postal addresss are set, this will return `null`.
+   *
+   * @returns {CustomerPostalAddressEntity|null} The primary postal address entity or null.
+   */
+  getPrimary() {
+    const { data } = this;
+    if (!data.length) return null;
+    const primary = data.find(({ AddressContactType }) => AddressContactType === 100);
+    if (primary) return primary;
+    const withCompany = data.find(({ Company }) => Company);
+    return withCompany || data[0];
+  }
 }
 
 module.exports = CustomerPostalAddressesResponse;
