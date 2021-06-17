6c3876b91b7c5b051ae61e76c13c7453da4235b0
Jacob Bare <jacob@parameter1.com>
2021-06-17 14:18:58 -0500

    Add `time` prop and getter helpers to error

--- packages/api-client/src/response/error.js
+++ packages/api-client/src/response/error.js
@@ -1,4 +1,4 @@
-const { get } = require('@parameter1/utils');
+const { get, getAsArray, getAsObject } = require('@parameter1/utils');
 
 class ApiResponseError extends Error {
   /**
@@ -6,13 +6,45 @@ class ApiResponseError extends Error {
    * @param {object} params
    * @param {object} params.json The parsed response JSON body
    * @param {object} params.fetchResponse The Fetch response
+   * @param {number} params.time The time (in MS) it took to retrieve the response.
    */
-  constructor({ json, fetchResponse } = {}) {
+  constructor({ json, fetchResponse, time } = {}) {
     const message = get(json, 'Errors.0.Error', fetchResponse.statusText);
     super(message);
     this.status = fetchResponse.status || 500;
     this.json = json;
     this.fetchResponse = fetchResponse;
+    this.time = time;
+  }
+
+  /**
+   * Gets a value, via dot-notation, from the parsed JSON response body.
+   *
+   * @param {string} path The dot-notated object path.
+   * @param {*} def The default value if undefined.
+   */
+  get(path, def) {
+    return get(this.json, path, def);
+  }
+
+  /**
+   * Gets a value, via dot-notation, from the parsed JSON response body
+   * and forces the value to an array.
+   *
+   * @param {string} path The dot-notated object path.
+   */
+  getAsArray(path) {
+    return getAsArray(this.json, path);
+  }
+
+  /**
+   * Gets a value, via dot-notation, from the parsed JSON response body
+   * and forces the value to an object.
+   *
+   * @param {string} path The dot-notated object path.
+   */
+  getAsObject(path) {
+    return getAsObject(this.json, path);
   }
 }
 
