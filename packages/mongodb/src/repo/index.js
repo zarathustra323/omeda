d85dc154730c14cfccdc8cfadb17e574dae76823
Jacob Bare <jacob@parameter1.com>
2021-06-17 14:04:42 -0500

    Add param validation and force brand to lower case

--- packages/mongodb/src/repo/index.js
+++ packages/mongodb/src/repo/index.js
@@ -1,13 +1,17 @@
+const Joi = require('@parameter1/joi');
 const BrandRepo = require('./brand');
 const BrandDemographicRepo = require('./brand-demographic');
 const BrandDeploymentTypeRepo = require('./brand-deployment-type');
 const BrandProductRepo = require('./brand-product');
 
-module.exports = ({
-  brandKey,
-  client,
-  dbName = 'omeda-api-data',
-}) => {
+module.exports = (params = {}) => {
+  const { value, error } = Joi.object({
+    brandKey: Joi.string().trim().lowercase().required(),
+    client: Joi.object().required(),
+    dbName: Joi.string().trim().default('omeda-api-data'),
+  }).validate(params);
+  const { brandKey, client, dbName } = value;
+  if (error) throw new Error(error.details[0].message);
   const brandDemographic = new BrandDemographicRepo({ brandKey, client, dbName });
   const brandDeploymentType = new BrandDeploymentTypeRepo({ brandKey, client, dbName });
   const brandProduct = new BrandProductRepo({ brandKey, client, dbName });
