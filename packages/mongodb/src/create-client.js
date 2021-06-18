b53bc4d5479385d2b9ee61c4296ac09847272c37
Jacob Bare <jacob@parameter1.com>
2021-06-18 07:19:02 -0500

    Add (optional) mongodb query logging

--- packages/mongodb/src/create-client.js
+++ packages/mongodb/src/create-client.js
@@ -1,7 +1,22 @@
+const { Logger } = require('@parameter1/mongodb');
 const MongoDBClient = require('@parameter1/mongodb/client');
 const { name, version } = require('../package.json');
 
-module.exports = ({ url } = {}) => new MongoDBClient({
+const { log } = console;
+
+module.exports = ({
   url,
-  options: { appname: `${name} v${version}` },
-});
+  enableQueryLogging,
+} = {}) => {
+  if (enableQueryLogging) {
+    Logger.setLevel('debug');
+    Logger.filter('class', ['Cursor']);
+    Logger.setCurrentLogger((message, context) => {
+      log(context.message);
+    });
+  }
+  return new MongoDBClient({
+    url,
+    options: { appname: `${name} v${version}` },
+  });
+};
