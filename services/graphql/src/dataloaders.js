67499de0c79279f267d4ec63c249e7bcca536006
Jacob Bare <jacob@parameter1.com>
2021-06-24 11:30:59 -0500

    Create `customerApi` data loader

--- services/graphql/src/dataloaders.js
+++ services/graphql/src/dataloaders.js
@@ -15,6 +15,19 @@ module.exports = ({ apiClient, repos } = {}) => {
     return customerIds.map((customerId) => map.get(customerId));
   };
 
+  const createCustomerApiLoader = () => async (customerIds) => {
+    const resource = apiClient.resource('customer');
+    const responses = await Promise.all(customerIds.map(async (customerId) => {
+      const r = await resource.lookupById({ customerId, errorOnNotFound: false });
+      return r;
+    }));
+    const map = responses.reduce((m, response) => {
+      m.set(response.data.Id, response);
+      return m;
+    }, new Map());
+    return customerIds.map((customerId) => map.get(customerId));
+  };
+
   const createRepoLoader = ({ name }) => async (ids) => {
     const repo = repos[name];
     if (!repo) throw new Error(`No MongoDB repo found for ${name}`);
@@ -31,6 +44,8 @@ module.exports = ({ apiClient, repos } = {}) => {
     brandDemographics: new DataLoader(createRepoLoader({ name: 'brandDemographic' })),
     brandDeploymentTypes: new DataLoader(createRepoLoader({ name: 'brandDeploymentType' })),
     brandProducts: new DataLoader(createRepoLoader({ name: 'brandProduct' })),
+
+    customerApi: new DataLoader(createCustomerApiLoader()),
 
     customerDemographics: new DataLoader(createCustomerRelLoader({ method: 'lookupDemographics' })),
     customerEmails: new DataLoader(createCustomerRelLoader({ method: 'lookupEmails' })),
