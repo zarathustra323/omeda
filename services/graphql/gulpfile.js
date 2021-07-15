6736553950e012e4785fc4ebc4c37f70bbec4d8e
Jacob Bare <jacob@parameter1.com>
2021-07-15 07:50:53 -0500

    Add api client package to watch paths

--- services/graphql/gulpfile.js
+++ services/graphql/gulpfile.js
@@ -2,5 +2,5 @@ const gulpfile = require('@parameter1/gulp/factory');
 
 gulpfile({
   entry: 'src/index.js',
-  watchPaths: ['src/**/*.js'],
+  watchPaths: ['src/**/*.js', '../../packages/api-client/src/**/*.js'],
 });
