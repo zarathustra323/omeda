738a5e822afab602e4a5e07976bd9398a93a40d2
Jacob Bare <jacob@parameter1.com>
2021-08-10 08:49:06 -0500

    Force empty values array for non-choice demographics

--- packages/api-client/src/entities/brand/demographic.js
+++ packages/api-client/src/entities/brand/demographic.js
@@ -4,8 +4,12 @@ const schema = require('../../schema/brand/demographic-elements');
 
 const DemographicValue = require('./demographic-value');
 
-const builder = ({ name, value }) => {
+const builder = ({ name, value, parent }) => {
   if (name === 'DemographicValues') {
+    // force an empty array for non-choice demographics.
+    // the Omeda API returns an array with a single value object, containing
+    // an Id of 0 (which is invalid)
+    if (![1, 2].includes(parent.DemographicType)) return [];
     return asArray(value).map((obj) => new DemographicValue(obj));
   }
   return null;
