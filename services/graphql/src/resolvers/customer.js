108f83983a2209d4279b145d5ce5a276a22bd532
Josh Worden <josh@parameter1.com>
2022-01-28 13:49:52 -0600

    Set value in map

--- services/graphql/src/resolvers/customer.js
+++ services/graphql/src/resolvers/customer.js
@@ -243,7 +243,7 @@ module.exports = {
       const Products = [{ OmedaProductId: input.productId }];
 
       const deploymentTypeIdMap = input.deploymentTypeIds.reduce((map, id) => {
-        map.set(id, { optedIn: true });
+        map.set(id, true);
         return map;
       }, new Map());
 
@@ -272,7 +272,7 @@ module.exports = {
       })();
 
       productDeploymentTypeMap.forEach((deploymentTypeId, productId) => {
-        const { optedIn } = deploymentTypeOptInMap.get(deploymentTypeId);
+        const optedIn = deploymentTypeOptInMap.get(deploymentTypeId);
         if (optedIn == null) return;
         Products.push({ OmedaProductId: productId, Receive: Number(optedIn) });
       });
