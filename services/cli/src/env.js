1a88437218f3dc8b0c01bdcfa35d3710cff3c4f1
Josh Worden <josh@parameter1.com>
2022-07-25 10:20:42 -0500

    Remove CLI

--- services/cli/src/env.js
+++
@@ -1,8 +0,0 @@
-const { cleanEnv, str } = require('envalid');
-
-module.exports = cleanEnv(process.env, {
-  OMEDA_APP_ID: str({ desc: 'The Omeda Application ID' }),
-  OMEDA_INPUT_ID: str({ desc: 'The Omeda Input ID' }),
-  OMEDA_BRAND_KEY: str({ desc: 'The Omeda Brand Key' }),
-  OMEDA_CLIENT_KEY: str({ desc: 'The Omeda Client Key' }),
-});
