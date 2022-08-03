d2550fe2c4c7c7958973e71e7b14588423449b20
Josh Worden <josh@parameter1.com>
2022-08-03 10:53:30 -0500

    Change to pass through to parent class

--- packages/api-cache-ioredis/src/index.js
+++ packages/api-cache-ioredis/src/index.js
@@ -2,8 +2,8 @@ const Redis = require('ioredis');
 const OmedaApiCacheInterface = require('@parameter1/omeda-api-client/cache-interface');
 
 class OmedaApiRedisCache extends OmedaApiCacheInterface {
-  constructor({ settings }, ...rest) {
-    super(...rest);
+  constructor({ settings, ...rest }) {
+    super({ ...rest });
     this.redis = new Redis(settings);
   }
 
